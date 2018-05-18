6e8dd09d80f34d1278a40ebde1aa1eafe82af817
cosiek <witold.majchrzak@21s.pl>
2018-05-19 00:14:08 +0200

    USe goquery to parse html

--- src/goComics/main.go
+++ src/goComics/main.go
@@ -4,21 +4,41 @@ import (
 	"io/ioutil"
 	"fmt"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 
-func getHTML(url string)[]byte{
+func getResponse(url string) *http.Response {
 	fmt.Println("Ściągam " + url)
 	resp, err := http.Get(url)
 	if err != nil { panic(err) }
+	// TODO: check response status
+	return resp
+}
+
+
+func getHTML(url string)[]byte{
+	resp := getResponse(url)
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, _ := ioutil.ReadAll(resp.Body)
 	return body
 }
 
 
+func getDocument(url string) (*goquery.Document, error){
+	resp := getResponse(url)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	return doc, err
+}
+
+
 func main() {
-	url := "https://gobyexample.com/"
-	body := getHTML(url)
-	ioutil.WriteFile("output.txt", body, 0644)
+	url := "http://buttersafe.com/"
+	doc, err := getDocument(url)
+	if err != nil { panic(err) }
+	doc.Find("img").Each(func(i int, s *goquery.Selection){
+		fmt.Println(s.Attr("src"))
+	})
+	fmt.Println("Done.")
 }
