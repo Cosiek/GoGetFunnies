95dddd81717d7252aba8af24cbee2c264d13c61b
cosiek <witold.majchrzak@21s.pl>
2018-05-19 12:52:52 +0200

    Move some functions to external file

---
+++ src/goComics/helpers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+
+func GetResponse(url string) *http.Response {
+	fmt.Println("Ściągam " + url)
+	resp, err := http.Get(url)
+	if err != nil { panic(err) }
+	// TODO: check response status
+	return resp
+}
+
+
+func GetHTML(url string)[]byte{
+	resp := GetResponse(url)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return body
+}
+
+
+func GetDocument(url string) (*goquery.Document, error){
+	resp := GetResponse(url)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	return doc, err
+}
--- src/goComics/main.go
+++ src/goComics/main.go
@@ -1,41 +1,15 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
-	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 
-func getResponse(url string) *http.Response {
-	fmt.Println("Ściągam " + url)
-	resp, err := http.Get(url)
-	if err != nil { panic(err) }
-	// TODO: check response status
-	return resp
-}
-
-
-func getHTML(url string)[]byte{
-	resp := getResponse(url)
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	return body
-}
-
-
-func getDocument(url string) (*goquery.Document, error){
-	resp := getResponse(url)
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	return doc, err
-}
-
-
 func main() {
 	url := "http://buttersafe.com/"
-	doc, err := getDocument(url)
+	doc, err := GetDocument(url)
 	if err != nil { panic(err) }
 	doc.Find("img").Each(func(i int, s *goquery.Selection){
 		fmt.Println(s.Attr("src"))
