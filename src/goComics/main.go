7a64c4dc14320f0c8e2a26127dbe3d46e773a413
cosiek <witold.majchrzak@21s.pl>
2018-05-28 01:00:16 +0200

    Template use "hello world"

--- src/goComics/main.go
+++ src/goComics/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"text/template"
 	"time"
 )
 
@@ -18,5 +20,13 @@ func main() {
 	for key, f := range definitions{
 		fmt.Println(key, f(date))
 	}
+	fmt.Println("Rendering output")
+	templ, err := template.ParseFiles("main_template.html")
+	if err != nil { panic(err) }
+	outFile, err := os.Create("komiksy.htm")
+	if err != nil { panic(err) }
+	err = templ.Execute(outFile, date)
+	if err != nil { panic(err) }
+	outFile.Close()
 	fmt.Println("Done")
 }
